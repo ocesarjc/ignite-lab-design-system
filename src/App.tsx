baccbe93663cdf6d9bef29e6e77ed4d0c869a897
Julio Cesar Rodrigues <99340770+ocesarjc@users.noreply.github.com>
2022-10-14 11:16:32 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App(): JSX.Element {
+  return (
+   <>
+   <h1 className='font-bold text-2xl text-violet-500'>Hello world</h1>
+   
+   <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-black hover:bg-cyan-300' >
+     Enviar
+     </button></>
+  )
+}
+
+export default App
